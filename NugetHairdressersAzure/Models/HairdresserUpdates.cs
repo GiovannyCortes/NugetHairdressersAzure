f8bc78a6280edc9015b1227fc5d5f677dc25e342
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-05-10 18:42:31 +0200

    v2.6.1

---
+++ NugetHairdressersAzure/Models/HairdresserUpdates.cs
@@ -0,0 +1,10 @@
+﻿namespace CutAndGo.Models {
+    public class HairdresserUpdates {
+        public int HairdresserId { get; set; }
+        public string Name { get; set; }
+        public string? Phone { get; set; }
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public string? Image { get; set; }
+    }
+}
--- NugetHairdressersAzure/Models/UserUpdates.cs
+++ NugetHairdressersAzure/Models/UserUpdates.cs
@@ -1,10 +1,9 @@
 ﻿namespace CutAndGo.Models {
-    public class UserRegister {
+    public class UserUpdates {
         public string Name { get; set; }
         public string? LastName { get; set; }
         public string? Phone { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string? Image { get; set; }
     }
 }
