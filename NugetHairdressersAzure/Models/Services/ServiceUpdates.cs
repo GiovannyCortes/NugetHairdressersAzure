64821df569c1993bfd231e3bec32cc30902c4a34
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-05-13 17:58:12 +0200

    v2.7.1

---
+++ NugetHairdressersAzure/Models/Services/ServiceUpdates.cs
@@ -0,0 +1,8 @@
+﻿namespace CutAndGo.Models {
+    public class ServiceUpdates {
+        public int ServiceId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public byte TiempoAprox { get; set; }
+    }
+}
