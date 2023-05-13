3a1d3df0c094ef4b7dcdd4fdfdfe34d009b2b8a6
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-05-13 16:55:31 +0200

    v2.7.0

--- NugetHairdressersAzure/Models/AppointmentUpdates.cs
+++ NugetHairdressersAzure/Models/AppointmentUpdates.cs
@@ -1,8 +1,6 @@
 ﻿namespace CutAndGo.Models {
     public class AppointmentUpdates {
         public int AppointmentId { get; set; }
-        public int UserId { get; set; }
-        public int HairdresserId { get; set; }
         public DateTime Date { get; set; }
         public TimeSpan Time { get; set; }
         public StatusAppointment Status { get; set; }
